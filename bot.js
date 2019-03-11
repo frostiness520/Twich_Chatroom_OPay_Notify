62c990a7cd441d9b1afcda60d50e11e1161a9ef6
Frosty_Sun <frostiness520@gmail.com>
2019-03-11 23:39:18 +0800

    Refine code to load config from conf.json

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 const TwitchBot = require('twitch-bot')
 var request = require('request');
+var config = require('./conf.json');
 var channelJoined = false
 var handledList = []
 var waitingList = []
+var setting = config[config.targetSettingFile]
+
 const Bot = new TwitchBot({
-  username: 'UserNameHere',
-  oauth: 'oauth get from https://twitchapps.com/tmi/',
-  channels: ['#vocaljudy']
+  username: setting.username,
+  oauth: setting.oath,
+  channels: [setting.channel]
 })
 
 Bot.on('join', channel => {
@@ -14,26 +17,32 @@ Bot.on('join', channel => {
     channelJoined = true
 })
 
+Bot.on('part', channel => {
+    console.log(`part channel: ${channel}`)
+    channelJoined = false
+    setTimeout(function(){Bot.join(setting.channel)},3000)
+})
+
 Bot.on('error', err => {
   console.log(err)
 })
 
 Bot.on('message', chatter => {
-  if(chatter.message === '!test') {
-    Bot.say('Command executed! PogChamp')
+  if(chatter.username === setting.modName && chatter.message === setting.testCommand) {
+    Bot.say('運作中')
   }
 })
 
 
 function donateCheaker() {
     request.post(
-        'https://payment.opay.tw/Broadcaster/CheckDonate/0D1F1C6F1F9A13F07BC733D53732DE9C',
+        'https://payment.opay.tw/Broadcaster/CheckDonate/' + setting.OPayKey,
         { json: { key: 'value' } },
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 if(body.lstDonate.length > 0){
                     body.lstDonate.forEach(function(element) {
-                        if(channelJoined && !handledList.includes(element.donateid)){
+                        if(!handledList.includes(element.donateid)){
                             handledList.push(element.donateid)
                             waitingList.push(element)
                         }
@@ -45,7 +54,7 @@ function donateCheaker() {
 }
 
 function msgSender(){
-    if(waitingList.length > 0){
+    if(waitingList.length > 0 && channelJoined){
         element = waitingList[0]
         bot_msg = "" + element.name + " 贊助了$" + element.amount + "，" + element.msg
         Bot.say(bot_msg)
