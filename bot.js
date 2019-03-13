887a737ebc3634459fb61b44bb039cddcc75be97
Frosty_Sun <frostiness520@gmail.com>
2019-03-14 01:57:02 +0800

    Support streamlabs notify

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const TwitchBot = require('twitch-bot')
 var request = require('request');
 var config = require('./conf.json');
+const Entities = require('html-entities').AllHtmlEntities;
+const entities = new Entities();
 var channelJoined = false
+var OPayObserverStatus = true
+var streamlabsObserverStatus = true
 var handledList = []
 var waitingList = []
+var msgTemplate = '{name} 贊助了{amount}'
 var setting = config[config.targetSettingFile]
 
 const Bot = new TwitchBot({
@@ -29,10 +34,28 @@ Bot.on('error', err => {
 
 Bot.on('message', chatter => {
   if(chatter.username === setting.modName && chatter.message === setting.testCommand) {
-    Bot.say('運作中')
+    Bot.say(`運行狀態=>OPay:${OPayObserverStatus}, Paypal:${streamlabsObserverStatus}`)
   }
 })
 
+//init streamLabs donate list
+request.get(
+    'https://www.twitchalerts.com/api/donations?access_token=' + setting.streamLabsToken,
+    { json: { key: 'value' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            body.donations.forEach(function(element) {
+                if(!handledList.includes(element.id)){
+                    handledList.push(element.id)
+                }
+            }, this);
+            streamlabsObserverStatus = true
+        }
+        else{
+            streamlabsObserverStatus = false
+        }
+    }
+)
 
 function donateCheaker() {
     request.post(
@@ -40,14 +63,45 @@ function donateCheaker() {
         { json: { key: 'value' } },
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
+                msgTemplate = entities.decode(body.settings.MsgTemplate)
                 if(body.lstDonate.length > 0){
                     body.lstDonate.forEach(function(element) {
                         if(!handledList.includes(element.donateid)){
                             handledList.push(element.donateid)
-                            waitingList.push(element)
+                            var temp = {}
+                            temp.name = element.name
+                            temp.amount = "TWD " + element.amount
+                            temp.msg = element.msg
+                            waitingList.push(temp)
                         }
                     })
                 }
+                OPayObserverStatus = true
+            }
+            else{
+                OPayObserverStatus = false
+            }
+        }
+    )
+    request.get(
+        'https://www.twitchalerts.com/api/donations?access_token=' + setting.streamLabsToken,
+        { json: { key: 'value' } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                body.donations.forEach(function(element) {
+                    if(!handledList.includes(element.id)){
+                        handledList.push(element.id)
+                        var temp = {}
+                        temp.name = element.donator.name
+                        temp.amount = element.currency + element.amount_label.replace('$', ' ')
+                        temp.msg = element.message
+                        waitingList.push(temp)
+                    }
+                }, this);
+                streamlabsObserverStatus = true
+            }
+            else{
+                streamlabsObserverStatus = false
             }
         }
     )
@@ -56,11 +110,13 @@ function donateCheaker() {
 function msgSender(){
     if(waitingList.length > 0 && channelJoined){
         element = waitingList[0]
-        bot_msg = "" + element.name + " 贊助了$" + element.amount + "，" + element.msg
+        bot_msg = msgTemplate.replace('{name}', element.name)
+        bot_msg = bot_msg.replace('{amount}', element.amount)
+        bot_msg = "/me " + bot_msg + " " + element.msg
         Bot.say(bot_msg)
         waitingList.splice(0, 1)
     }
 }
   
-setInterval(donateCheaker, 1500);
+setInterval(donateCheaker, 2000);
 setInterval(msgSender, 2000);
