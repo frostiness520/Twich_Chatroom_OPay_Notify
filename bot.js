f687be48ddb4b0dd7a8ce6bed71279a752a178b6
Frosty_Sun <frostiness520@gmail.com>
2019-06-24 23:26:40 +0800

    Modify kernel module from twitch-bot to tmi.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const TwitchBot = require('twitch-bot')
+const tmi = require("tmi.js");
 var request = require('request');
 var config = require('./conf.json');
 const puppeteer = require('puppeteer');
@@ -17,15 +17,28 @@ var msgTemplate = '{name} 贊助了{amount}'
 var setting = config[config.targetSettingFile]
 var viewers = 0
 var hours = '0'
+var title = ''
 
-const Bot = new TwitchBot({
-  username: setting.username,
-  oauth: setting.oath,
-  channels: [setting.channel]
-})
+let options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+    identity: {
+        username: setting.username,
+        password: setting.oath
+    },
+    channels: [ setting.channel ]
+};
+const client = new tmi.client(options);
+client.connect();
 
 viewersChecker()
 queryHours()
+checker()
 
 function getSubTier(subPlan){
     if(subPlan.indexOf('1000') >= 0){
@@ -56,80 +69,92 @@ function pushCheckIgnoreCount(msg, gifter){
     waitingList.push(msg)
 }
 
-Bot.on('join', channel => {
-    console.log(`Joined channel: ${channel}`)
-    channelJoined = true
-})
+client.on("join", (channel, username, self) => {
+    if(!self)
+      return;
+      console.log(`Joined channel: ${channel}`)
+      channelJoined = true
+  });
 
-Bot.on('part', channel => {
-    console.log(`part channel: ${channel}`)
+client.on("disconnected", (reason) => {
+    console.log('disconnected')
     channelJoined = false
-    setTimeout(function(){Bot.join(setting.channel)},3000)
-})
+});
 
-Bot.on('error', err => {
-  console.log(err)
-})
-
-Bot.on('message', chatter => {
-  if(chatter.username === setting.modName && chatter.message === setting.testCommand) {
-    Bot.say(`運行狀態=>OPay:${OPayObserverStatus}, Paypal:${streamlabsObserverStatus}`)
-  }
-  else if(chatter.message === '!人數'){
-    let shouldPush = true
-    waitingList.forEach(function(element){
-        if(element.includes('目前聊天室人數')){
-            shouldPush = false
+client.on("message", (channel, userstate, message, self) => {
+    if(userstate["message-type"] == "action" || userstate["message-type"] == "chat"){
+        if(userstate["username"] === setting.modName && message === setting.testCommand) {
+            client.say(setting.channel, `運行狀態=>OPay:${OPayObserverStatus}, Paypal:${streamlabsObserverStatus}`);
         }
-    })
-    if(shouldPush){
-        waitingList.push(`目前聊天室人數:${viewers}`)
+        else if(message === '!人數'){
+            let shouldPush = true
+            waitingList.forEach(function(element){
+                if(element.includes('目前聊天室人數')){
+                    shouldPush = false
+                }
+            })
+            if(shouldPush){
+                waitingList.push(`目前聊天室人數:${viewers}`)
+            }
+        }
+        else if(message === '!本月時數'){
+            let shouldPush = true
+            waitingList.forEach(function(element){
+                if(element.includes('已實況')){
+                    shouldPush = false
+                }
+            })
+            if(shouldPush){
+                let t = new Date()
+                let y = t.getTime() - 3600000
+                let yt = new Date(y)
+                let s =  (yt.getMonth()+1) + '/' + yt.getDate()
+                waitingList.push(`累積至${s}的現在為止，已實況了${hours}小時`)
+            }    
+        }
+        else if(message === '!標題'){
+            let shouldPush = true
+            waitingList.forEach(function(element){
+                if(element.includes('實況標題:')){
+                    shouldPush = false
+                }
+            })
+            if(shouldPush){
+                waitingList.push(`實況標題: ${title}`)
+            }
+        }
     }
-  }else if(chatter.message === '!本月時數'){
-    let shouldPush = true
-    waitingList.forEach(function(element){
-        if(element.includes('已實況')){
-            shouldPush = false
-        }
-    })
-    if(shouldPush){
-        let t = new Date()
-        let y = t.getTime() - 3600000
-        let yt = new Date(y)
-        let s =  (yt.getMonth()+1) + '/' + yt.getDate()
-        waitingList.push(`累積至${s}的現在為止，已實況了${hours}小時`)
-    }    
-  }
-})
 
-Bot.on('gift', event => {
-    var tier = getSubTier(String(event.msg_param_sub_plan))
-    msg = `${event.display_name} 送了一份層級 ${tier} 訂閱給 ${event.msg_param_recipient_display_name} (${event.msg_param_recipient_user_name})！他/她已經在本頻道送出了 ${event.msg_param_sender_count} 份贈禮訂閱！`
+});
+
+client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
+    console.log('subgift')
+    let tier = getSubTier(String(methods["plan"]))
+    let senderCount = ~~userstate["msg-param-sender-count"];
+    msg = `${username} 送了一份層級 ${tier} 訂閱給 ${userstate["msg-param-recipient-display-name"]} (${userstate["msg-param-recipient-user-name"]})！他/她已經在本頻道送出了 ${senderCount} 份贈禮訂閱！`
     if(ignoreCount > 0){
-        gifteeList.push(`${event.msg_param_recipient_display_name} (${event.msg_param_recipient_user_name})`)
+        gifteeList.push(`${userstate["msg-param-recipient-display-name"]} (${userstate["msg-param-recipient-user-name"]})`)
         ignoreCount--
         return
     }
     waitingList.push(msg)
-})
+});
 
-Bot.on('continueSub', event => {
-    msg = `${event.display_name} 將繼續使用 ${event.msg_param_sender_name} 贈送的贈禮訂閱!`
-    waitingList.push(msg)
-})
-
-Bot.on('subscription', event => {
-    console.log('subscription plan:' + event.msg_param_sub_plan)
-})
-
-Bot.on('mysterygift', event => {
+client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
+    console.log('mysterygift')
+    let senderCount = ~~userstate["msg-param-sender-count"];
     gifteeList = []
-    var tier = getSubTier(String(event.msg_param_sub_plan))
-    ignoreCount = event.msg_param_mass_gift_count
+    let tier = getSubTier(String(methods["plan"]))
+    ignoreCount = numbOfSubs
     setTimeout(function(){ignoreCount = 0}, 10000)
-    msg = `${event.display_name} 送了 ${event.msg_param_mass_gift_count} 份層級 ${tier} 訂閱給社群！他/她已經在本頻道送出了 ${event.msg_param_sender_count} 份贈禮訂閱！`
-    pushCheckIgnoreCount(msg, event.display_name)
-})
+    msg = `${username} 送了 ${numbOfSubs} 份層級 ${tier} 訂閱給社群！他/她已經在本頻道送出了 ${senderCount} 份贈禮訂閱！`
+    pushCheckIgnoreCount(msg, username)
+});
+
+client.on("giftpaidupgrade", (channel, username, sender, userstate) => {
+    msg = `${username} 將繼續使用 ${sender} 贈送的贈禮訂閱!`
+    waitingList.push(msg)
+});
 
 //init streamLabs donate list
 request.get(
@@ -211,12 +236,12 @@ function donateCheaker() {
 function msgSender(){
     if(waitingList.length > 0 && channelJoined){
         let bot_msg = waitingList[0]
-        Bot.say('/me ' + bot_msg)
+        client.say(setting.channel, '/me ' + bot_msg);
         waitingList.splice(0, 1)
         if(pmList.length > 0){
             setTimeout(function(){
                 bot_msg = pmList[0]
-                Bot.say('/w ' + config.targetSettingFile + ' ' + bot_msg)
+                client.say(setting.channel, '/w ' + config.targetSettingFile + ' ' + bot_msg);
                 pmList.splice(0, 1)
             },1000)
         }
@@ -228,15 +253,39 @@ function viewersChecker(){
         'http://tmi.twitch.tv/group/user/vocaljudy/chatters',
         { json: { key: 'value' } },
         function (error, response, body) {
-            viewers = body.chatter_count
+            if (!error && response.statusCode == 200) {
+                if (body.chatter_count) {
+                    viewers = body.chatter_count
+                }
+            }
         }
     )
 }
 
+function checker(){
+    request({
+      headers: {
+        'Client-ID': '4z6vya336cojem0mpzdqqzbghkv23j',
+        'Accept': 'application/vnd.twitchtv.v5+json'
+      },
+      uri: 'https://api.twitch.tv/kraken/channels/162505127',
+      method: 'GET'
+    }, function (err, res, body) {
+      if (!err && res.statusCode == 200) {
+        let channel = JSON.parse(body)
+        if (channel && channel.status) {
+            title = channel.status
+        }
+      }
+    });
+};
+
+
+
 async function queryHours(){
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
     const page = await browser.newPage();
-    await page.goto('https://twitchtracker.com/vocaljudy/statistics');
+    await page.goto('https://twitchtracker.com/vocaljudy/statistics', {waitUntil: 'load', timeout: 60000});
     
     let $ = cheerio.load(await page.content())
     let data = []
@@ -251,3 +300,4 @@ setInterval(donateCheaker, 2000);
 setInterval(msgSender, 2000);
 setInterval(viewersChecker, 30000);
 setInterval(queryHours, 600000);
+setInterval(checker, 10000);
